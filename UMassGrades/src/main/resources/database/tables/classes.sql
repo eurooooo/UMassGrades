ea31b093382d381802e8b1ac032dacbdeb4e02be
jennarbates <jennaraquelbates@gmail.com>
2024-11-09 18:02:04 -0500

    moved all tables into one file

--- UMassGrades/src/main/resources/database/tables/classes.sql
+++
@@ -1,7 +0,0 @@
--- This script creates the classes table with class_id, class_name, and class_description columns
-
-CREATE TABLE classes (
-                         class_id SERIAL PRIMARY KEY,           -- Unique identifier for each class
-                         class_name VARCHAR(100) NOT NULL,      -- Name of the class, required field
-                         class_description TEXT                 -- Description of the class, optional field
-);
--- UMassGrades/src/main/resources/database/tables/professors.sql
+++
@@ -1,7 +0,0 @@
--- create_professors_table.sql
--- This script creates the professors table with professor_id and professor_name columns
-
-CREATE TABLE professors (
-                            professor_id SERIAL PRIMARY KEY, -- Unique identifier for each professor
-                            professor_name VARCHAR(100) NOT NULL -- Name of the professor, required field
-);
