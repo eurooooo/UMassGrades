8de7c2b5b48f7a4a4f5d2e79663c3c8e9467996c
Zephyr Lin <lzh910954163@gmail.com>
2024-11-08 23:57:04 -0500

    interac with backend

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
 import InitialHome from "@/components/InitialHome";
 import Image from "next/image";
 
-export default function page() {
-  return <InitialHome />;
+export default async function page() {
+  const res = await fetch("http://localhost:8080/api/classes");
+  const data = await res.json();
+
+  return (
+    <ul>
+      {data.map((pro) => (
+        <li key={pro.id}>
+          <p>
+            id:{pro.id} name: {pro.name}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+  // <InitialHome />;
 }
